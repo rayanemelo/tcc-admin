524f76d4ed5bb54a8eaea86c411f90ad8fba83f4
raymeloc <raymeloc@gmail.com>
2026-01-09 15:55:35 -0300

    components

---
+++ src/app/page.tsx
@@ -0,0 +1,8 @@
+"use client";
+export default function Home() {
+  return (
+    <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans ">
+      <h1>teste</h1>
+    </div>
+  );
+}
